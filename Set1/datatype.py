03be1bc64a786a8bcc27c1870ddea6baa0bbcace
KULDEEP SINGH NEGI <nkuldeep615@gmail.com>
2023-08-05 10:56:01 +0530

    datatype

---
+++ Set1/datatype.py
@@ -0,0 +1,18 @@
+x = 5
+print(type(x))
+
+name = "Alice"
+print(type(name))  
+
+is_student = True
+print(type(is_student))
+
+fruits = ["apple", "banana", "cherry"]
+print(type(fruits))
+
+coordinates = (5, 3)
+print(type(coordinates))
+
+person = {"name": "Alice", "age": 30, "city": "New York"}
+print(type(person))
+
