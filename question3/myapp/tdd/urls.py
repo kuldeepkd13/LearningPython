5c9146961e37bea2b09241aa771c5d149f1886cd
KULDEEP SINGH NEGI <nkuldeep615@gmail.com>
2023-08-21 18:21:41 +0530

    tdd

--- question3/myapp/tdd/urls.py
+++ question3/myapp/tdd/urls.py
@@ -4,6 +4,6 @@ from tdd import views
 
 urlpatterns = [
  path("view/<str:id>/" , views.view  , name="view"),
- path("create" , views.create  , name="create"),
- path("delete" , views.delete  , name="delete")
+#  path("create" , views.create  , name="create"),
+#  path("delete" , views.delete  , name="delete")
 ]
--- question3/myapp/tdd/views.py
+++ question3/myapp/tdd/views.py
@@ -3,4 +3,14 @@ from django.http import JsonResponse
 import json
 # Create your views here.
 
+def view(request,id):
+    user_data = {
+        '1':{'username':"rahul", 'caption':"create"},
+        '2':{'username':"roma", 'caption':"create"}
+    }
+
+    data = user_data.get(id)
+    if data is None:
+        return JsonResponse({'error':"No data"}, status= 404)
+    return JsonResponse(data)
 
